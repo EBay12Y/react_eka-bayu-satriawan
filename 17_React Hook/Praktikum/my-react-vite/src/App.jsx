201dfc62911c66a5c05579f7189abae4b16e96dc
EBay12Y <ekabayus12@gmail.com>
2023-03-18 18:57:03 +0700

    add praktikum

---
+++ 17_React Hook/Praktikum/my-react-vite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateProduct from './components/views/CreateProduct.view';
+
+function App() {
+
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  )
+}
+
+export default App
