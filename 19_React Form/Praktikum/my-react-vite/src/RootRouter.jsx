622e2cb7b6adf8be7d83e10bed40a75744241662
EBay12Y <ekabayus12@gmail.com>
2023-03-25 10:46:36 +0700

    add praktikum

--- 19_React Form/Praktikum/my-react-vite/src/RootRouter.jsx
+++ 19_React Form/Praktikum/my-react-vite/src/RootRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CreateProduct, LandingPage, ProductDetail, SignupForm, Counter, CreateProductFormik } from './components/views'
+import { CreateProduct, LandingPage, ProductDetail, SignupForm, Counter, CreateProductFormik, SignupFormik, LoginFormik } from './components/views'
 
 const RootRouter = () => {
     return (
@@ -10,6 +10,8 @@ const RootRouter = () => {
                 <Route path="/createproduct" element={<CreateProduct />} />
                 <Route path="/createproductformik" element={<CreateProductFormik />} />
                 <Route path="/detailproduct/:id" element={<ProductDetail />} />
+                <Route path="/signupformik" element={<SignupFormik />} />
+                <Route path="/loginformik" element={<LoginFormik />} />
                 <Route path="/signup" element={<SignupForm />} />
                 <Route path="/counter" element={<Counter />} />
             </Routes>
--- 19_React Form/Praktikum/my-react-vite/src/components/views/index.js
+++ 19_React Form/Praktikum/my-react-vite/src/components/views/index.js
@@ -3,4 +3,6 @@ export { default as CreateProductFormik } from "./CreateProductFormik.view"
 export { default as LandingPage } from "./LandingPage.view";
 export { default as ProductDetail } from "./DetailProduct.view"
 export { default as SignupForm } from "./SignupForm.view"
+export { default as SignupFormik } from "./SignupFormik.view"
+export { default as LoginFormik } from "./LoginFormik.view"
 export { default as Counter } from "./Counter.view"
